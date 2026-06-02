1e6b137e3a21f152a7fda6b0c69759e5547195fd
Anmol Arya <anmolarya@Anmols-MacBook-Pro.local>
2026-06-02 15:26:51 +0530

    wiring all the services with successful communication between them in the clusters

--- event-bus/index.js
+++ event-bus/index.js
@@ -14,19 +14,19 @@ app.post('/events', (req, res) => {
 
   events.push(event);
 
-  axios.post('http://localhost:4000/events', event).catch((err) => {
+  axios.post('http://posts-clusterip-srv:4000/events', event).catch((err) => {
     console.log('Error posting to posts service:', err.message);
   });
 
-  axios.post('http://localhost:4001/events', event).catch((err) => {
-    console.log('Error posting to posts service:', err.message);
+  axios.post('http://comments-srv:4001/events', event).catch((err) => {
+    console.log('Error posting to comments service:', err.message);
   });
 
-  axios.post('http://localhost:4002/events', event).catch((err) => {
+  axios.post('http://query-srv:4002/events', event).catch((err) => {
     console.log('Error posting to query service:', err.message);
   });
 
-  axios.post('http://localhost:4003/events', event).catch((err) => {
+  axios.post('http://moderation-srv:4003/events', event).catch((err) => {
     console.log('Error posting to moderation service:', err.message);
   });
 
